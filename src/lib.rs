4db40f351a80e7a180146d2114ed5598d71a776a
SpiralP <spiral.palmer@gmail.com>
2019-03-05 14:02:40 -0800

    test: track total download rate

--- src/lib.rs
+++ src/lib.rs
@@ -146,18 +146,33 @@ fn test_get_download_speed_stream() {
   actix::spawn(
     get_servers_sorted_by_ping()
       .and_then(|(servers, _pings)| {
+        use std::collections::HashMap;
+        use std::sync::{Arc, Mutex};
+        let map: Arc<Mutex<HashMap<String, f64>>> = Arc::new(Mutex::new(HashMap::new()));
+
         stream::iter_ok(servers)
           .take(4)
-          .and_then(|server| {
+          .and_then(move |server| {
+            let map = map.clone();
             Ok(futures::lazy(move || {
               get_download_speed_stream(server.clone()).and_then(move |stream| {
                 stream.for_each(move |(total_bytes, total_nanoseconds)| {
                   let rate = ((total_bytes as f64) / 1_000_000.0)
                     / ((total_nanoseconds as f64) / 1_000_000_000.0);
 
-                  info!("rate: {} {}", server, rate);
+                  let total = {
+                    let mut map = map.lock().unwrap();
+                    map.insert(server.clone(), rate);
 
-                  actix::System::current().stop();
+                    let mut total = 0.0;
+                    for n in map.values() {
+                      total += n;
+                    }
+                    total
+                  };
+
+                  info!("total rate: {}", total);
+                  info!("rate: {} {}", server, rate);
 
                   Ok(())
                 })
